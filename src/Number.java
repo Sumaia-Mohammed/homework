953314b6877350629e51faee31c3be9092f9134b
Sumaia-Mohammed <sasysosy5@hotmail.com>
2019-10-22 01:06:33 +0200

    hi

---
+++ src/Number.java
@@ -0,0 +1,12 @@
+public abstract class ExerciseIntegers {
+    private int number ;
+
+    public Number (int number ){
+        this.number = number;
+    }
+
+    public Integer getNumber (){
+        return number;
+    }
+
+}
